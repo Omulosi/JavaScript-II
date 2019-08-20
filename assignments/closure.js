3c1548c1706a0a2289e7fce5c6007033417342cb
Omulosi <mulongojohnpaul@gmail.com>
2019-08-20 15:49:33 +0300

    Complete solution to challenge 2 and 3: Closures

--- assignments/closure.js
+++ assignments/closure.js
@@ -16,10 +16,15 @@ function sayName(){
 const counterMaker = () => {
   // IMPLEMENTATION OF counterMaker:
   // 1- Declare a `count` variable with a value of 0. We will be mutating it, so declare it using `let`!
+  let count = 0;
   // 2- Declare a function `counter`. It should increment and return `count`.
   //      NOTE: This `counter` function, being nested inside `counterMaker`,
   //      "closes over" the `count` variable. It can "see" it in the parent scope!
+  function counter() {
+    return count++;
+  }
   // 3- Return the `counter` function.
+  return counter;
 };
 // Example usage: const myCounter = counterMaker();
 // myCounter(); // 1
@@ -29,6 +34,16 @@ const counterMaker = () => {
 // It should have a `limit` parameter. Any counters we make with `counterMaker`
 // will refuse to go over the limit, and start back at 1.
 
+const counterMaker2 = (limit) => {
+  let count = 0;
+  function counter() {
+    if (count >= limit) {
+      count = 0;
+    }
+    return ++count;
+  }
+  return counter;
+};
 // ==== Challenge 4: Create a counter function with an object that can increment and decrement ====
 const counterFactory = () => {
   // Return an object that has two methods called `increment` and `decrement`.

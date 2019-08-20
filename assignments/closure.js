67057c8370c1831fe9b7f8fb208a004cf6299b13
Omulosi <mulongojohnpaul@gmail.com>
2019-08-20 15:40:01 +0300

    Complete solution to challenge 1: Closures

--- assignments/closure.js
+++ assignments/closure.js
@@ -4,6 +4,10 @@
 // that manipulates variables defined in the outer scope.
 // The outer scope can be a parent function, or the top level of the script.
 
+let myName = 'Mulongo'
+function sayName(){
+  console.log(myName);
+}
 
 /* STRETCH PROBLEMS, Do not attempt until you have completed all previous tasks for today's project files */
 

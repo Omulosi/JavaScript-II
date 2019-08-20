a10eedbb11383e74261ce00e1067176c2c372c31
Omulosi <mulongojohnpaul@gmail.com>
2019-08-20 16:47:58 +0300

    Complete solution to challenge 4: Closures

--- assignments/closure.js
+++ assignments/closure.js
@@ -49,4 +49,17 @@ const counterFactory = () => {
   // Return an object that has two methods called `increment` and `decrement`.
   // `increment` should increment a counter variable in closure scope and return it.
   // `decrement` should decrement the counter variable and return it.
+  let count = 0;
+
+  counter = {
+    increment() {
+      return count++;
+    },
+
+    decrement() {
+      return count--;
+    },
+  }
+
+  return counter;
 };
